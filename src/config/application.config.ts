f36ea2395045e0b17428fce95bcf48a700694da1
Andrey Romanov <64421792+quick6response@users.noreply.github.com>
2024-06-10 09:33:07 +0300

    init project

---
+++ src/config/application.config.ts
@@ -0,0 +1,11 @@
+export const ApplicationConfig = {
+  port: 3010,
+  db: {
+    host: 'localhost',
+    port: 5432,
+    user: 'postgres',
+    password: 'postgres',
+    database: 'postgres'
+  },
+  
+}
