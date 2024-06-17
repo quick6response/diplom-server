b9270587473599d830819be1508a4c3faafc0926
Andrey Romanov <64421792+quick6response@users.noreply.github.com>
2024-06-17 20:58:18 +0300

    Обновлен example

--- config/application.config.example.ts
+++ config/application.config.example.ts
@@ -1,9 +1,17 @@
 export const ApplicationConfig = {
   db: {
+    dialect: 'postgres',
     host: 'localhost',
     port: 5432,
     user: 'postgres',
-    password: 'postgres',
-    database: 'postgres',
+    password: 'root',
+    database: 'SystemEducation',
+  },
+  auth: {
+    secret: 'secret',
+    // 60s
+    assessTokenTime: 60,
+    // 30d
+    refreshTokenTime: 60 * 60 * 24 * 30,
   },
 };
